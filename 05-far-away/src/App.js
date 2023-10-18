e6424daf6601225f724d658bb30972a824cfef14
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-18 22:36:20 +0300

    lesson 70 - Building the layout

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -1,9 +1,37 @@
-function App() {
+export default function App() {
     return (
         <div className="App">
-            Hello
+            <Logo/>
+            <Form/>
+            <PackingList/>
+            <Stats/>
         </div>
     );
 }
 
-export default App;
+function Logo() {
+    return <h1>🌴 Far Away 💼</h1>
+}
+
+function Form() {
+    return (
+        <div className={'add-form'}>
+          <h3>What do you need for your 😍 trip?</h3>
+        </div>)
+}
+
+function PackingList() {
+ return (
+    <div className={'packing-list'}>
+      LIST
+    </div>)
+}
+
+function Stats() {
+    return (
+        <footer className={'stats'}>
+            <em>
+                💼 You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>)
+}
