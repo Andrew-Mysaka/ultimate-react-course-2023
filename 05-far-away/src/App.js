38186aaa5edc187361a7fed77216fa535917cea3
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-19 20:29:59 +0300

    lesson 72 - building a form and handling submissions

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -20,10 +20,23 @@ function Logo() {
 }
 
 function Form() {
+    function handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
     return (
-        <div className={'add-form'}>
-          <h3>What do you need for your 😍 trip?</h3>
-        </div>)
+        <form className={'add-form'} onSubmit={handleSubmit}>
+            <h3>What do you need for your 😍 trip?</h3>
+            <select>
+                {Array.from({length:20}, (_, i) => i+1).map((num) => (
+                    <option value={num} key={num}>{num}</option>
+                ))}
+            </select>
+            <input type="text" placeholder="Item..."/>
+            <button>Add</button>
+        </form>
+    )
 }
 
 function PackingList() {
@@ -31,7 +44,7 @@ function PackingList() {
         <div className={'list'}>
             <ul>
                 {initialItems.map((item) => (
-                    <Item item={item}/>
+                    <Item item={item} key={item.id}/>
                 ))}
             </ul>
         </div>
