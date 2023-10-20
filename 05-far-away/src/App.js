669dff572497dbf4f229921de4ead43f78904f99
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-20 22:39:15 +0300

    lesson 86 - sorting item

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -61,13 +61,31 @@ function Form({onAddItems}) {
 }
 
 function PackingList({items, onDeleteItems, onToggleItem}) {
+    const [sortBy, setSortBy] = useState('packed');
+
+    let sortedItems;
+
+    if(sortBy === 'input') sortedItems = items;
+
+    if(sortBy === 'description') sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+
+    if(sortBy === 'packed') sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+
     return (
         <div className={'list'}>
             <ul>
-                {items.map((item) => (
+                {sortedItems.map((item) => (
                     <Item item={item} key={item.id} onDeleteItem={onDeleteItems} onToggleItem={onToggleItem}/>
                 ))}
             </ul>
+
+            <div className={'actions'}>
+                <select value={setSortBy} onChange={e=>setSortBy(e.target.value)}>
+                    <option value="input">Sort by input order</option>
+                    <option value="description">Sort by description</option>
+                    <option value="packed">Sort by packed status</option>
+                </select>
+            </div>
         </div>
     )
 }
