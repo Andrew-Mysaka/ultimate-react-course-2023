b1391b5aee027bb72398ae61009821b6fff1cc8c
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-22 21:44:59 +0300

    Lesson 98 - Displaying the new Friend form

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -21,13 +21,25 @@ const initialFriends = [
     },
 ];
 
+function Button({children, onClick}){
+    return (
+        <button className={'button'} onClick={onClick}>{children}</button>
+    );
+}
+
 export default function App() {
+    const [showAddFriend,setShowAddFriend] = useState(false);
+
+    function handleShowAddFriend(){
+        setShowAddFriend(!showAddFriend);
+    }
+
     return (
         <div className={'app'}>
             <div className={'sidebar'}>
                 <FriendsList/>
-                <FormAddFriend/>
-                <Button>Add friend</Button>
+                {showAddFriend && <FormAddFriend/>}
+                <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
             </div>
 
             <FormSplitBill/>
@@ -58,12 +70,6 @@ function Friend({friend}){
 
             <Button>Select</Button>
         </li>
-    );
-}
-
-function Button({children}){
-    return (
-        <button className={'button'}>{children}</button>
     );
 }
 
