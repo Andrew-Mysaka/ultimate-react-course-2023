90decbf8466be8cd816c5df381a2c84e63aa3af0
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-16 23:33:48 +0200

    Lesson 276 Back to Thunks

--- 15-redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/src/features/accounts/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
     reducers: {
         deposit(state, action) {
             state.balance += action.payload;
+            state.isLoading = false;
         },
         withdraw(state, action) {
             state.balance -= action.payload;
@@ -32,15 +33,31 @@ const accountSlice = createSlice({
             state.loanPurpose = action.payload.purpose;
             state.balance += action.payload.amount;
         }},
-        payLoan(state, action){
+        payLoan(state){
             state.loanPurpose = "";
             state.balance -= state.loan;
             state.loan = 0;
+        },
+        convertingCurrency(state){
+            state.isLoading = true;
         }
     }
 })
 
-export const {deposit, withdraw, requestLoan, payLoan} = accountSlice.actions;
+export const {withdraw, requestLoan, payLoan} = accountSlice.actions;
+
+export function deposit(amount, currency) {
+    if(currency === "USD") return {type: "account/deposit"}
+
+    return async function (dispatch, getState){
+        dispatch({type: "account/convertingCurrency", payload: amount})
+        const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        dispatch({type: "account/deposit", payload: converted});
+    }
+}
 
 export default accountSlice.reducer;
 
